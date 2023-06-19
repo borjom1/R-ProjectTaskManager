5b2c261f19cd21c6b7db8f07a90845906088f43f
Igor <ig.zuban1ch@gmail.com>
2023-06-19 15:00:22 +0300

    feat: add configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,20 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  important: '#root',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-1a': '#1A1A1A',
+        'dark-26': '#262626',
+        'dark-24': '#242424',
+        'dark-31': '#313131',
+        'dark-42': '#424242',
+        'white-f0': '#F0F0F0',
+        'white-b': '#bbbbbb',
+        'cyan': '#3AA6AD'
+      }
+    },
   },
   plugins: [],
 }
