7b77286fb54c824eba54c46f1f9621d17fe797f9
Igor <ig.zuban1ch@gmail.com>
2023-06-22 15:08:15 +0300

    feat(authApi): add refresh request

--- src/services/api/authApi.js
+++ src/services/api/authApi.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL + '/auth';
 
 export const register = async body => {
   try {
-    const response = await axios.post(`${API_URL}/auth/register`, body);
+    const response = await axios.post(`${API_URL}/register`, body);
     return response.data;
   } catch ({response}) {
     return response.data;
@@ -13,7 +13,18 @@ export const register = async body => {
 
 export const login = async body => {
   try {
-    const response = await axios.patch(`${API_URL}/auth/login`, body);
+    const response = await axios.patch(`${API_URL}/login`, body);
+    return response.data;
+  } catch ({response}) {
+    return response.data;
+  }
+};
+
+export const refresh = async refresh => {
+  try {
+    const response = await axios.patch(`${API_URL}/refresh`, {
+      token: refresh
+    });
     return response.data;
   } catch ({response}) {
     return response.data;
