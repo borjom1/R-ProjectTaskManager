f8d01b946b2c0b8bd79779aa7c0bad47e8f4f2da
Igor <ig.zuban1ch@gmail.com>
2023-06-23 02:00:17 +0300

    feat(Index): pull all user projects using api

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,63 +1,74 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useNavigate} from "react-router-dom";
 import PageTemplate from "../components/PageTemplate";
 import ProjectList from "../components/ProjectList";
 import UserProfile from "../components/UserProfile";
+import NewProject from '../components/NewProject';
 import CreateNewFolderRoundedIcon from '@mui/icons-material/CreateNewFolderRounded';
+import UndoIcon from '@mui/icons-material/Undo';
 import {Button} from "@mui/material";
-import {getUser} from "../utils/localstorage";
+import {getUser, saveUser} from "../utils/localstorage";
+import {getProjects} from "../services/api/projectsApi";
+import {refresh} from '../services/api/authApi';
 
 export default function Index() {
 
   const navigate = useNavigate();
+  const [userRoles, setUserRoles] = useState([]);
+  const [projects, setProjects] = useState([]);
+  const [isCreateOpened, setCreateOpened] = useState(false);
 
   useEffect(() => {
-    const item = getUser();
-    if (!item) {
+    const user = getUser();
+    if (!user) {
       navigate('/sign_in');
     }
-  }, []);
 
-  const projects = [
-    {
-      id: 1,
-      title: 'Mobile Banking App',
-      desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-      people: 12,
-      tasksDone: 30,
-      tasksUnDone: 3
-    },
-    {
-      id: 2,
-      title: 'Mobile Banking App',
-      desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-      people: 8,
-      tasksDone: 12,
-      tasksUnDone: 17
-    },
-  ];
+    getProjects(user.access).then(res => {
+      if (res.status && res.status === 401) {
+        refresh(user.refresh).then(data => {
+          saveUser(data);
+          getProjects(data.access).then(data => setProjects(data));
+        });
+      } else {
+        setProjects(res);
+      }
+    });
+
+  }, []);
 
   return (
     <PageTemplate
-      upperRight={<UserProfile/>}>
+      upperRight={<UserProfile setUserRoles={setUserRoles}/>}>
       <div className='w-full h-full bg-dark-24 rounded-[30px]'>
         <div className='h-[10%] w-full bg-dark-31 rounded-t-[30px] flex items-center justify-between py-4 px-12'>
           <p className='text-white-d6 text-2xl font-medium'>Projects you are involved in...</p>
-          <Button
-            variant="outlined"
-            startIcon={<CreateNewFolderRoundedIcon/>}
-            style={{
-              borderWidth: 3,
-              borderColor: '#0f735f',
-              color: '#19b096',
-            }}>
-            New project
-          </Button>
+          {userRoles.includes('MANAGER') ?
+            <Button
+              variant="outlined"
+              onClick={() => setCreateOpened(!isCreateOpened)}
+              startIcon={isCreateOpened ? <UndoIcon/> : <CreateNewFolderRoundedIcon/>}
+              style={{
+                borderWidth: 3,
+                borderColor: '#0f735f',
+                color: '#19b096',
+              }}>
+              {isCreateOpened ? 'Back' : 'New project'}
+            </Button>
+            : null
+          }
         </div>
-        <ProjectList
-          className='flex flex-col gap-4 h-[90%] px-12 py-6 overflow-y-auto'
-          projects={projects}
-        />
+        {isCreateOpened ?
+          <NewProject
+            setCreateOpened={setCreateOpened}
+            projects={projects}
+            setProjects={setProjects}
+          /> :
+          <ProjectList
+            className='flex flex-col gap-4 h-[90%] px-12 py-6 overflow-y-auto'
+            projects={projects}
+          />
+        }
       </div>
     </PageTemplate>
   );
