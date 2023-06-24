3febde1adad8f72ca6438d7f0cf7ab867a5e1bcf
Igor <ig.zuban1ch@gmail.com>
2023-06-24 23:30:44 +0300

    feat(App): add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Index from "./pages/Index";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import Project from "./pages/Project";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Index/>}/>
+        <Route path='/project/:id' element={<Project/>}/>
         <Route path='/sign_up' element={<SignUp/>}/>
         <Route path='/sign_in' element={<SignIn/>}/>
+        <Route path='*' element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
   )
