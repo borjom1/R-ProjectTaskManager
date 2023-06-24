5ee12f26ae1c803b792ef101531b4a1133ce0418
Igor <ig.zuban1ch@gmail.com>
2023-06-24 23:25:45 +0300

    feat: add temporary page for not found routes

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className='bg-dark-21 w-full h-full h-[100%] flex items-center'>
+      <p className='w-full text-2xl font-medium text-center text-white-f0'>Not found page</p>
+    </div>
+  );
+};
+
+export default NotFound;
