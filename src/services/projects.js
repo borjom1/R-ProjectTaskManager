427cf4c9c49f24e3ed1eb2e73ed16f67033ed3d5
Igor <ig.zuban1ch@gmail.com>
2023-06-24 23:26:57 +0300

    feat: create service for projects

---
+++ src/services/projects.js
@@ -0,0 +1,22 @@
+import {getStories} from "./api/projectsApi";
+import {refresh} from "./api/authApi";
+import {getUser, saveUser} from "../utils/localstorage";
+
+export const pullAllStories = async (projectId) => {
+  const user = getUser();
+  const response = await getStories(projectId, user.access);
+
+  if (response.status) { // error is present
+    if (response.status === 401) { // unauthorized
+
+      const newAuthData = await refresh(user.refresh);
+      saveUser(newAuthData);
+      return getStories(projectId, newAuthData.access);
+
+    } else if (response.status === 403) { // not project I am taking part in
+      throw 'forbidden';
+    }
+  } else {
+    return getStories(projectId, user.access);
+  }
+};
