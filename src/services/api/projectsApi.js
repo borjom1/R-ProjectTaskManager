7f978020894c039a318ac5eec538e01cf7e0ec48
Igor <ig.zuban1ch@gmail.com>
2023-06-24 23:26:38 +0300

    feat(projectApi): add requests for retrieving stories and tasks

--- src/services/api/projectsApi.js
+++ src/services/api/projectsApi.js
@@ -2,13 +2,26 @@ import axios from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL + '/user/projects';
 
+const authHeader = (token) => {
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+};
+
+export const getProject = async (id, accessToken) => {
+  try {
+    const response = await axios.get(`${API_URL}/${id}`, authHeader(accessToken));
+    return response.data;
+  } catch ({response}) {
+    return response;
+  }
+};
+
 export const getProjects = async accessToken => {
   try {
-    const response = await axios.get(API_URL, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    });
+    const response = await axios.get(API_URL, authHeader(accessToken));
     return response.data;
   } catch ({response}) {
     return response;
@@ -17,11 +30,35 @@ export const getProjects = async accessToken => {
 
 export const createProject = async (body, accessToken) => {
   try {
-    const response = await axios.post(API_URL, body, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    });
+    const response = await axios.post(API_URL, body, authHeader(accessToken));
+    return response.data;
+  } catch ({response}) {
+    return response;
+  }
+};
+
+export const getFirstStory = async (projectId, accessToken) => {
+  try {
+    const response = await axios.get(`${API_URL}/${projectId}/story`, authHeader(accessToken));
+    return response.data;
+  } catch ({response}) {
+    console.log(response)
+    return response;
+  }
+};
+
+export const getStories = async (projectId, accessToken) => {
+  try {
+    const response = await axios.get(`${API_URL}/${projectId}/stories`, authHeader(accessToken));
+    return response.data;
+  } catch ({response}) {
+    return response;
+  }
+};
+
+export const getTasks = async (projectId, storyId, accessToken) => {
+  try {
+    const response = await axios.get(`${API_URL}/tasks?projectId=${projectId}&storyId=${storyId}`, authHeader(accessToken));
     return response.data;
   } catch ({response}) {
     return response;
