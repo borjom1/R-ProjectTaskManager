d111dd1ba911bc7536a74bcfc8748367c3e3d789
Igor <ig.zuban1ch@gmail.com>
2023-06-25 12:19:03 +0300

    refactor(StoryItem): change icons

--- src/components/StoryItem.jsx
+++ src/components/StoryItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {FaListUl} from 'react-icons/fa';
-import {CgTimelapse} from 'react-icons/cg';
+import {AiOutlineCalendar} from 'react-icons/ai';
 import classNames from "classnames";
 
 const StoryItem = ({id, name, tasks, from, to, onClick}) => {
@@ -15,12 +15,12 @@ const StoryItem = ({id, name, tasks, from, to, onClick}) => {
       <p className='text-white-f0 text-xl'>{name}</p>
       <div className='flex gap-6 items-center'>
         <div className='flex gap-2 items-center'>
-          <FaListUl size={16} color={'#838383'}/>
+          <FaListUl size={17} color={'#838383'}/>
           <p className='text-[#838383]'>{tasks}</p>
         </div>
         <div className='flex gap-1 items-center'>
-          <CgTimelapse size={18} color={'#515151'}/>
-          <p className='text-[#515151]'>{`${from.replaceAll('-', '.')} - ${to.replaceAll('-', '.')}`}</p>
+          <AiOutlineCalendar size={20} color={'#515151'}/>
+          <p className='text-[#515151]'>{`${from?.replaceAll('-', '.')} - ${to?.replaceAll('-', '.')}`}</p>
         </div>
       </div>
     </div>
